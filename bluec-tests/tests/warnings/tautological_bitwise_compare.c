9ea6c0d8a882bb7d193846f2c650ca85b54f5add
Neil Henderson <neil@bluetarpmedia.com>
2026-02-28 16:24:43 +1000

    Add warning test case for tautological-bitwise-compare

---
+++ bluec-tests/tests/warnings/tautological_bitwise_compare.c
@@ -0,0 +1,24 @@
+int main(void) {
+    int a = 1, b = 0;
+    b = a | 2;        // No warning
+    b = 2 | a;        // No warning
+    b = a & (a | 2);  // No warning
+
+    // Boolean context but Bitwise OR with zero
+    b = a && (a | 0); // No warning
+    b = a && (0 | a); // No warning
+
+    // Boolean context and Bitwise OR with non-zero
+    //
+
+    b = a && (a | 2);
+    b = (2 | a) || a;
+
+    b = !(a | 2);
+
+    _Bool result = 0;
+    result = (_Bool)(a | 2);  // No warning
+    result = a | 2;
+
+    return 0;
+}
